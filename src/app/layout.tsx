fe3d15942793ff6aa7507c10dab7516388088d7a
Michał Konopski <58469680+misiekhardcore@users.noreply.github.com>
2024-02-04 10:57:22 +0100

    chore: set up linters (#1)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,11 @@
-import type { Metadata } from "next";
+import type { Metadata } from 'next';
 
-import "./globals.scss";
+import './globals.scss';
 
 export const metadata: Metadata = {
-  title: "Next.js Markdown Blog",
+  title: 'Next.js Markdown Blog',
   description:
-    "This is a blog/portfolio website built using Nextjs 13 in which all the content is stored as markdown files.",
+    'This is a blog/portfolio website built using Nextjs 13 in which all the content is stored as markdown files.',
 };
 
 export default function RootLayout({
